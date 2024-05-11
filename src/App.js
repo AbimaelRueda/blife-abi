a2cffd3d820bcfbe614d42f6515697627c8680e0
AbimaelRueda <85275635+AbimaelRueda@users.noreply.github.com>
2024-05-11 05:23:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header.jsx';
+import Body from './Components/Body.jsx';
+import Card from './Components/Card.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      {/* <Body/> */}
+      <br/>
+    </div>
+  );
+}
+
+export default App;
